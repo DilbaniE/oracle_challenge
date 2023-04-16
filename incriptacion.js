d4d07511bb1c7ffa1300deafcf0da6ea70077023
DilbaniE <claudiaenriquez241@gmail.com>
2023-04-16 18:18:35 -0500

    add styles

--- incriptacion.js
+++ incriptacion.js
@@ -1,5 +1,6 @@
 let btnEncriptar = document.getElementById("botonEncriptador");
 let btnDesencriptar = document.getElementById("botonDesencriptar");
+let btnCopiar = document.getElementById("botoCopiar")
 let iconMuneco = document.getElementById("icon-muneco");      
 let textH3 = document.getElementById("text-h3");
 let textP = document.getElementById("text-p");
@@ -8,7 +9,11 @@ let area_text = document.getElementById("pregunta");
 function encriptar(){
     
     var texodilba = area_text.value
-    .replace(/e/gi, "enter");
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ia")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat")
 
     
     if(texodilba.length > 0)
@@ -20,7 +25,7 @@ function encriptar(){
     else
     {
         alert("Digitas porfavor algo para ser encriptado");
-        textH3.display = "block"
+        textH3.display = "flex"
         iconMuneco.src = "./assets/img/muneco.svg"
     }
     textH3.innerText=texodilba ;
@@ -28,6 +33,40 @@ function encriptar(){
 
 
 
-btnEncriptar.onclick = encriptar; 
+function desencriptar(){
     
+    var texodilba = area_text.value
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ia/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u")
 
+    
+    if(texodilba.length > 0)
+    {
+        alert(texodilba);
+        textH3.display = "none"
+        iconMuneco.src = "./assets/img/good.png" 
+    }
+    else
+    {
+        alert("Digitas porfavor algo para ser encriptado");
+        textH3.display = "flex"
+        iconMuneco.src = "./assets/img/muneco.svg"
+    }
+    textH3.innerText=texodilba ;
+}   
+
+
+
+function copiar(){
+    alert("copiado");
+    navigator.clipboard.writeText(textH3.textContent)
+    btnCopiar.textContent = "ya copiado!"
+
+}
+
+btnEncriptar.onclick = encriptar; 
+btnDesencriptar.onclick = desencriptar;
+btnCopiar.onclick = copiar;
